3167e5255f7773df28052259c577aa873138d997
Fernando Gross <gross51@outlook.com>
2020-08-14 00:13:54 -0300

    Adds Toast component to notify activities finished

---
+++ src/components/Toast.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{ 'show': content.show }" class="toast top-right">
+    <div class="toast-header">
+      {{ content.header }}
+      <button type="button" class="close pull-right" data-dismiss="toast" @click="content.show = false">&times;</button>
+    </div>
+    <div class="toast-body">
+      {{ content.body }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toast',
+  props: {
+    content: { required: true }
+  },
+  methods: {
+    fadeToast () {
+      setTimeout(() => {
+        this.content.show = false
+      }, 2000)
+    }
+  },
+  updated () {
+    if (this.content.show) {
+      this.fadeToast()
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .pull-right {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .top-right {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+</style>
